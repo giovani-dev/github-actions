ac32007f5bdb9346e6cd8d01a7c6a495ca424433
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2022-11-23 13:49:48 -0300

    feat: src

---
+++ tests/test_hello_word.py
@@ -0,0 +1,12 @@
+class TestHelloWorld:
+    def test_print_call_01(self):
+        assert True
+
+    def test_print_call_02(self):
+        assert True
+
+    def test_print_call_03(self):
+        assert True
+
+    def test_print_call_04(self):
+        assert True
